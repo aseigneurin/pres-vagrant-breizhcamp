79d7498e16280ec96b07a767c5f87184cb9fcca7
Alexis Seigneurin <alexis.seigneurin@gmail.com>
2014-05-20 01:18:49 +0200

    - configuration to work without an internet connection

---
+++ sans_internet/0_packaging_boxes/scripts/provision.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+set -x
+
+apt-get -y update
+#apt-get -y install apache2
+apt-get -y install nginx
---
+++ sans_internet/1_simple_vagrant_vm/scripts/provision.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+set -x
+
+#apt-get -y update
+apt-get -y install nginx
